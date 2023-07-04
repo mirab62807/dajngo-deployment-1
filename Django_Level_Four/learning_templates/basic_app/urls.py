1b63e04f31f61f2b2b90d6fc22ef214e05cf1118
mirab62807 <98186194+mirab62807@users.noreply.github.com>
2023-07-04 02:03:32 +0100

    Update urls.py

--- Django_Level_Four/learning_templates/basic_app/urls.py
+++ Django_Level_Four/learning_templates/basic_app/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 from basic_app import views
 
 # SET THE NAMESPACE!
 app_name = 'basic_app'
 
 urlpatterns=[
-    url(r'^relative/$',views.relative,name='relative'),
-    url(r'^other/$',views.other,name='other'),
+    path('relative/',views.relative,name='relative'),
+    path('^other/',views.other,name='other'),
 ]
